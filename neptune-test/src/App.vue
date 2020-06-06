693ee0adfff40fbdc35e0cfb4d883e872f4d9de7
Henry Neal <henry.neal0@gmail.com>
2020-06-06 13:52:40 -0400

    initial setup for vue and added packages, working on setting up store

---
+++ neptune-test/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+	<div id="app">
+		<Quotes />
+	</div>
+</template>
+
+<script>
+import Quotes from "./components/Quotes.vue";
+export default {
+	name: "App",
+	components: {
+		Quotes,
+	},
+	data() {
+		return {
+			quotes: [],
+		};
+	},
+};
+</script>
+
+<style>
+#app {
+	font-family: Avenir, Helvetica, Arial, sans-serif;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+	text-align: center;
+	color: #2c3e50;
+	margin-top: 60px;
+}
+</style>
---
+++ neptune-test/src/components/Quotes.vue
@@ -0,0 +1,13 @@
+<template>
+	<div>
+		<h3>Quotes</h3>
+	</div>
+</template>
+
+<script>
+export default {
+	name: "Quotes",
+};
+</script>
+
+<style></style>
